50a5a4299ec7767a1088521e389100bbceed2eb2
Rafreaks06 <mr.rafreaks@gmail.com>
2025-05-25 06:31:47 +0700

    perbaikan pada proses login

--- src/routes/authRoutes.go
+++ src/routes/authRoutes.go
@@ -2,14 +2,14 @@ package routes
 
 import (
 	"project-1/src/controllers"
-
 	"github.com/gin-gonic/gin"
 )
 
-// AuthRoutes mendefinisikan semua endpoint yang berkaitan dengan autentikasi
+// AuthRoutes mengatur semua rute terkait autentikasi
 func AuthRoutes(router *gin.Engine) {
-	auth := router.Group("/api") // Prefix untuk semua route autentikasi
+	auth := router.Group("/api")
 	{
-		auth.POST("/login", controllers.Login) // Endpoint untuk proses login pengguna
+		auth.POST("/register", controllers.Register) // Route untuk registrasi
+		auth.POST("/login", controllers.Login)       // Route untuk login
 	}
 }
